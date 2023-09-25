7109e770f005ec6f82ffbe960b55063c2a68f696
shyamkryadav <shyamkryadav345@gmail.com>
2023-09-25 18:35:07 +0545

    get all input data in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  productName: string;
+  productCategory: string;
+  productPrice: string;
+  productDescription: string;
+  productEnterDate: string;
+  productExpireDate: string;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,9 +17,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formtest';
-  productData:any={}
-
-  addProductDetials(){
-    console.log(this.productData)
+  productData:Product={
+    productName: '',
+    productCategory: '',
+    productPrice: '',
+    productDescription: '',
+    productEnterDate : '',
+    productExpireDate:''
   }
+
+   
+  productsList: Product={
+    productName: '',
+    productCategory: '',
+    productPrice: '',
+    productDescription: '',
+    productEnterDate: '',
+    productExpireDate: ''
+  };
+
+
+  ngOnInit():void{
+    console.log(this.productsList);
+
+  }
+
+  addProductDetails(){
+    this.productsList=this.productData
+    console.log(this.productsList);
+    
+  // Save the updated productsList to localStorage
+
+    
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
